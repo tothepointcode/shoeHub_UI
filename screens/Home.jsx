acba02c1f37c10c9de001153297626b9afb0dc4a
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-09-17 00:10:38 +0000

    Feat: Tile placeholder for shoes added.

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 // Styled components
 import { StyledContainer, colors } from "./../styles/shared";
 import styled from "styled-components/native";
 
-const { tint, gray } = colors;
+const { primary, tint, gray } = colors;
 
 const SectionView = styled.View`
   flex-direction: row;
   justify-content: space-between;
+  margin-bottom: 15px;
 `;
 
 const SectionText = styled.Text`
   font-size: 20px;
-  letter-spacing: 0.5
+  letter-spacing: 0.5px;
   color: ${gray};
 `;
 
@@ -24,6 +26,36 @@ const Underline = styled.View`
   width: 30%;
   background-color: ${tint};
   height: 4px;
+`;
+
+const ShowCase = styled(ScrollView)`
+  width: 100%;
+  margin-top: 10px;
+`;
+
+const ShoeItem = styled.View`
+  height: 300px;
+  background-color: ${gray};
+  width: 250px;
+  margin-right: 25px;
+  border-radius: 25px;
+  padding: 20px;
+`;
+
+const ItemHead = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const Title = styled.Text`
+  font-weight: bold;
+  color: ${primary};
+  padding-bottom: 5px;
+  font-size: 18px;
+`;
+
+const SubTitle = styled.Text`
+  color: ${primary};
 `;
 
 const Home = () => {
@@ -67,7 +99,30 @@ const Home = () => {
           );
         })}
       </SectionView>
-      <Text>{sections[activeSection].name}</Text>
+      {/* <Text>{sections[activeSection].name}</Text> */}
+      <ShowCase horizontal={true}>
+        <ShoeItem>
+          <ItemHead>
+            <View>
+              <Title>Kyrie 6</Title>
+              <SubTitle>$130.00</SubTitle>
+            </View>
+            <TouchableOpacity>
+              <AntDesign name="hearto" size={25} color={primary} />
+            </TouchableOpacity>
+          </ItemHead>
+          <View
+            style={{
+              backgroundColor: "white",
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Text>Image space</Text>
+          </View>
+        </ShoeItem>
+      </ShowCase>
     </StyledContainer>
   );
 };
